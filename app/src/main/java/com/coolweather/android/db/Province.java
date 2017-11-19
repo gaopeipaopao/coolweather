a8f6e78f2bdd8a5b148078ec7b5cc0ee0b0e27ce
gaopei <gaopeigpgp@126.com>
2017-11-19 22:50:50 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -16,6 +16,9 @@ public class Province extends DataSupport {
     public void setId(int id){
         this.id=id;
     }
+    public String getProvinceName(){
+        return provinceName;
+    }
     public void setProvinceName(String provinceName){
         this.provinceName=provinceName;
     }
